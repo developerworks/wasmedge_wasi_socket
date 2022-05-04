36dbe1ed24c2b8a613f51466d8e81a1b8376d498
developerworks <developerworks@163.com>
2022-05-05 02:14:32 +0800

    modify run.sh

--- examples/http_server/run.sh
+++ examples/http_server/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cargo build --target wasm32-wasi --release
-wasmedge target/wasm32-wasi/release/http_server.wasm
+cargo build --target wasm32-wasi --release && wasmedge target/wasm32-wasi/release/http_server.wasm
